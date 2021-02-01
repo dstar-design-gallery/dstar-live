e7809cf20de38408b7599e4eb1411c41854884ee
derisen <dogan.erisen@gmail.com>
2021-01-31 17:31:04 -0800

    first commit

---
+++ precache-manifest.3d8beb8e81b4c0613bb29984173107c2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a9baceb4ac3d7703a5964985257519a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f506841abb08dca8f9ca",
+    "url": "/static/css/2.d581d7f8.chunk.css"
+  },
+  {
+    "revision": "ea7a15d803e2126511bb",
+    "url": "/static/css/main.05097009.chunk.css"
+  },
+  {
+    "revision": "f506841abb08dca8f9ca",
+    "url": "/static/js/2.63695342.chunk.js"
+  },
+  {
+    "revision": "07312f0bb7caf26059f99ef68670122d",
+    "url": "/static/js/2.63695342.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "069a61ee973b0c9eed6e",
+    "url": "/static/js/3.70a1309c.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/3.70a1309c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a26381c3db716ce03a7",
+    "url": "/static/js/4.e84eeb9f.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/static/js/4.e84eeb9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a72b26fb790202ddfd47",
+    "url": "/static/js/5.9f91b867.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/static/js/5.9f91b867.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea7a15d803e2126511bb",
+    "url": "/static/js/main.0cf06205.chunk.js"
+  },
+  {
+    "revision": "80e289c3cc6c19467b4b",
+    "url": "/static/js/runtime-main.a5af6cf3.js"
+  },
+  {
+    "revision": "3fef72e04d9ddd8231f6468a85609923",
+    "url": "/static/media/DStaWhite.3fef72e0.png"
+  },
+  {
+    "revision": "d0e930823a19caf9646300d4a8181cac",
+    "url": "/static/media/DStar.d0e93082.png"
+  },
+  {
+    "revision": "e36ef145b8a9729b2b7c7810382a3758",
+    "url": "/static/media/SquamishThree1.e36ef145.png"
+  },
+  {
+    "revision": "d948da919878ef72ddc7864975100b4d",
+    "url": "/static/media/collections.d948da91.png"
+  }
+]);
